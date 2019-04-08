5e9a77ef9ee6db9e91496119c20432f6ff65340d
moaz sabbagh <moazsabbagh@moazs-MacBook-Pro.local>
2019-04-08 15:36:23 +0200

    change

--- animate.js
+++ animate.js
@@ -1,118 +1,3 @@
-// // // const shapes = document.querySelectorAll(".logo-svg polygon");
-// // // const stroke = document.querySelector("g");
-// // // const body = document.querySelector("body");
-// // // const svg = document.querySelector(".logo-svg");
-// // // const container = document.querySelector(".container");
-
-// // // // tl = new TimelineLite();
-// // // // tl.add("start");
-// // // // tl.staggerFrom(
-// // // //   shapes,
-// // // //   1,
-// // // //   {
-// // // //     opacity: 0,
-// // // //     cycle: {
-// // // //       x: [200, -200, -700, 700],
-// // // //       y: [20, -20, -200, 200],
-// // // //       rotation: function(i) {
-// // // //         return i * 200;
-// // // //       }
-// // // //     },
-
-// // // //     ease: Elastic.easeOut
-// // // //   },
-// // // //   0.001
-// // // // );
-
-// // // // tl.staggerTo(
-// // // //   shapes,
-// // // //   3,
-// // // //   {
-// // // //     cycle: {
-// // // //       x: [200, -200, -700, 700],
-// // // //       y: [200, -200, -200, 200],
-// // // //       rotation: function(i) {
-// // // //         return i * 40;
-// // // //       }
-// // // //     },
-// // // //     opacity: 0,
-// // // //     fill: "#f2bf30",
-// // // //     ease: Circ.easeInOut
-// // // //   },
-// // // //   0.001,
-// // // //   "start+=3"
-// // // // );
-
-// // // TweenMax.set(container, { perspective: 1000 });
-// // // TweenMax.set(svg, {
-// // //   transformStyle: "preserve-3d"
-// // // });
-
-// // // TweenMax.from(svg, 7, {
-// // //   // opacity: 0.3,
-
-// // //   // scale: 0.5,
-// // //   // opacity: 0,
-// // //   skewX: 40,
-// // //   rotationX: -200,
-// // //   rotationY: -200,
-// // //   rotationZ: -200,
-// // //   rotation: 360,
-// // //   transformOrigin: "50% 50% 50%  ",
-// // //   // rotationY: 360,
-
-// // //   // transformPerspective: 600,
-// // //   ease: Circ.easeInOut
-// // // });
-
-// // // // tl = new TimelineLite();
-// // // // tl.add("start");
-// // // // tl.staggerFrom(
-// // // //   shapes,
-// // // //   5,
-// // // //   {
-// // // //     cycle: {
-// // // //       // rotation: [100, 200, 300, 100],
-// // // //       opacity: [0.2, 0, 0.6, 0.5]
-// // // //       // scale: [0.2, 0.5, 0.6, 0.5]
-// // // //       // transformOrigin: "50% 50% 50% "
-// // // //     },
-
-// // // //     ease: Elastic.easeOut
-// // // //   },
-// // // //   0.001
-// // // // );
-
-// // function Student(name, age) {
-// //   this.name = name;
-// //   this.age = age;
-// // }
-
-// // console.log(Student.constructor);
-
-// // const firstS = new Student("moaz", 38);
-// // console.log(firstS);
-
-// // function add(c, d) {
-// //   console.log(this.a + this.b + c + d);
-// // }
-// // console.log(add(3, 4));
-
-// // function factorial(x) {
-// //   if (x < 0) return `${x} have to positive number`;
-// //   if (x === 0) return 1;
-// //   return x * factorial(x - 1);
-// // }
-
-// // console.log(factorial(1));
-
-// // function revString(str) {
-// //   if (str === "") return "";
-// //   return revString(str.substr(1)) + str[0];
-// // }
-
-// // console.log(revString("cat"));
-
 // MODEL code
 
 import { Ball } from "./ball.js";
@@ -142,9 +27,6 @@ function keyDownFn(e) {
   }
   if (e.keyCode === 40) {
     bat.velocityTop = 0.2;
-    if (bat.top + bat.height >= 800) {
-      bat.velocityTop = 0;
-    }
   }
 }
 
@@ -194,6 +76,16 @@ function updateFrame(t0) {
 
   bat.move(t0 - t1); // today
   bat1View.style.top = bat.top + "px";
+
+  if (bat.top + bat.height >= 800) {
+    bat.top = 800 - bat.height;
+
+    // container.style.backgroundColor = makeColor();
+  }
+  if (bat.top <= 0) {
+    bat.top = 0;
+    // container.style.backgroundColor = makeColor();
+  }
 
   t1 = t0;
 }
